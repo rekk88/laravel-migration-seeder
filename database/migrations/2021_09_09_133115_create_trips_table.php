a16359da4b8eb3d0df2bf25640bad6fb0e23c5f4
maicol <maicol.matteoli@gmail.com>
2021-09-09 15:47:21 +0200

    add : first migration woohoooooooooo

---
+++ database/migrations/2021_09_09_133115_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',80);
+            $table->string('destination',100);
+            $table->string('departure');
+            $table->string('days');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
