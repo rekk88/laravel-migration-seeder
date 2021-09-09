7ad76461e61afd402569af047b2d99c61314c3ee
maicol <maicol.matteoli@gmail.com>
2021-09-09 17:32:45 +0200

    add : route show

--- app/Http/Controllers/MigrationController.php
+++ app/Http/Controllers/MigrationController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Trip; //richiamo il model per poter leggere i dati dal db
 
 class MigrationController extends Controller
 {
   public function index(){
     return view('welcome');
+  }
+
+  public function showAll(){
+    $gite = Trip::all(); //carico il db in una variabile
+    // return view('show')->with('trips',$gite);
+    // dd($gite);
+    return view('show' , ['vacanze' => $gite]);
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','MigrationController@index');
+
+Route::get('/show','MigrationController@showAll')->name('show');
